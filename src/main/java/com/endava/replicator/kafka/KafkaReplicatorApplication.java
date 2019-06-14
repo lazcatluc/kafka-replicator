50dc2f2ce2a225fbfcf8a4819b53082c21a8786a
Catalin Lazar <catalin.lazar@endava.com>
2019-06-14 10:48:01 +0300

    Initial commit

---
+++ src/main/java/com/endava/replicator/kafka/KafkaReplicatorApplication.java
@@ -0,0 +1,13 @@
+package com.endava.replicator.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaReplicatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaReplicatorApplication.class, args);
+	}
+
+}
